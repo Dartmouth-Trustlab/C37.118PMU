ca01dbf074a10fc9fbd4821e3b9c22d07a378055
Super Mexico <rafael.brantley@gmail.com>
2017-09-01 19:56:37 -0400

    Run this to parse all .pcap's in "thePcaps" directory

--- C37/ProcessPcaps.sh
+++ C37/ProcessPcaps.sh
@@ -23,83 +23,29 @@ do
   echo -e "\nParsing..."
   echo $pcap | cut -d "/" -f 3
 
-  tshark -x -r $pcap | cut -c 7-55 > allhex.txt
+  editcap -C 66 $pcap "headlessPCAP.pcap"
+
+  tshark -x -r headlessPCAP.pcap | cut -c 7-55 > allhex.txt
   # Extracts the hex with the newlines between the frames
 
-  breaks=$(grep -nvE '[^[:space:]]' allhex.txt | sed s/://g)
-  # Gets a list of the line-numbers of the newline separations
 
-  # This will remove the TCP packets
-  # Iterations
-  # 1 - Compare break to start
-  # If the difference is exactly 5, replace the frame with empty lines
-  # 2 - Update start
-
-  start=1
-
-  offset=0
-  # This variable will account for the change in the number of lines when deleting
-
-  for break in $breaks
-  do
-    if [ $start -eq $(($break-$offset-5)) ]
-    then
-      	sed "$start",$(($break-$offset))d allhex.txt > tmp.txt
-		cat tmp.txt > allhex.txt
-		# Replaces the garbage lines with empty lines
-
-		(( offset += 6 ))
-
-    fi
-
-  start=$(($break + 1 - $offset))
-  # Moves the "start" pointer to the beginning of the next frame
-
-  done
-
-  # Here is the best place to trim the garbage
-  sed 's/ aa/\
+  sed 's/^aa/\
 +aa/' allhex.txt > tmp.txt
-  	cat tmp.txt > allhex.txt
+  cat tmp.txt > allhex.txt
+  # Puts a '+' in front of each SYNC byte to mark them
 
+  sed 1,$(($(grep -n -m 1 "\+" allhex.txt | cut -d : -f 1) - 1))d allhex.txt > tmp.txt
+  cat tmp.txt > allhex.txt
+  # Removes the first big couple of header packets
 
-  SYNCLines=$(grep -n "aa" allhex.txt | cut -d : -f 1)
+  SYNCLines=$(grep -n "\+aa" allhex.txt | cut -d : -f 1)
   # Gets the lines with the sync bytes in them
 
-
-  backup=1
-  # This will be used to delete the lines behind the SYNC line
-
- 
-  # For each SYNC line,
-  # Insert a newline before the SYNC byte
-  # Delete previous lines until an empty line
-
-  for Line in $SYNCLines
-  do
-
-  	# Until the deletion has backed up to either a line break or the end of the file...
-  	until [[ -z $(sed -n $(($Line-$backup))p allhex.txt) || $(($Line-$backup)) == 0 ]]
-  	do
-  		sed $(($Line-$backup))g allhex.txt > tmp.txt
-  		cat tmp.txt > allhex.txt
-
-  		((backup++))
-  	done
-
-  	let backup=1
-  	# Resets for the next SYNC line
-  done
-
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' allhex.txt | sed s/" "//g > fulltest.txt
-  # Removes all of the new lines and spaces
-  sed 's/+//' fulltest.txt > tmp.txt
-  # Deletes the first +
-  tr + '\n' <tmp.txt > fulltest.txt
-  # Replaces all subsequent ones with newlines
-  echo '' >> fulltest.txt
-  # Adds a final newline
-
+  sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' allhex.txt | sed s/" "//g  | sed 1's/\+//' | sed 's/\+/\
+/g' > fulltest.txt
+  # Removes the new lines and spaces
+  # Removes the first '+'
+  # Replaces the rest of the '+'s with newlines
 
   rm allhex.txt
   rm tmp.txt
@@ -108,4 +54,3 @@ do
   #Runs the parser!
 
 done
-
